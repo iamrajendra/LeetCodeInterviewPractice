3cdb7873b6086bb2258285771230f6f51d22adc0
iamrajendra <rajendrarajaram@hotmail.com>
2020-11-05 08:45:55 +0530

    Distint Array
    Given a sorted array of distinct integers and a target value, return the
    index if the target is found. If not, return the index where it would be
    if it were inserted in order.

--- src/DistintArray.java
+++ src/DistintArray.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+
+
 
 public class DistintArray {
+    static int mid=0;
+    public static void main(String[] args) {
+        int[] array  =  new int[]{1,3,5,6};
+        int x  =5;
+        int indexOfElement  =  binarySearch(array,x);
+        System.out.println("last index = " +indexOfElement );
+
+    }
+    private   static int binarySearch(int array[],int target){
+        int left =   0;
+        int right  = array.length-1;
+        Arrays.sort(array);
+        return  binarySearch(array,left,right,target);
+
+    }
+
+    static int binarySearch(int arr[], int left, int right, int x)
+    {
+        if (right >= left) {
+             mid = left + (right - left) / 2;
+
+            // If the element is present at the
+            // middle itself
+            if (arr[mid] == x)
+                return mid;
+
+            // If element is smaller than mid, then
+            // it can only be present in left subarray
+            if (arr[mid] > x)
+                return binarySearch(arr, left, mid - 1, x);
+
+            // Else the element can only be present
+            // in right subarray
+            return binarySearch(arr, mid + 1, right, x);
+        }
+
+        // We reach here when element is not present
+        // in array
+        return mid;
+    }
 }
