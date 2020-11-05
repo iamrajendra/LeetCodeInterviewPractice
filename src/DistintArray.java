1a12240da2233dcc54ba959b9ea4bc9bc8e4925c
iamrajendra <rajendrarajaram@hotmail.com>
2020-11-05 08:55:42 +0530

    Search A Distint element
    Given a sorted array of distinct integers and a target value, return the
    index if the target is found. If not, return the index where it would be
    if it were inserted in order.

--- src/DistintArray.java
+++ src/DistintArray.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class DistintArray {
+    public static void main(String[] args) {
+        int [] array  = new int[]{1,3,5,6};
+        System.out.println("args = " + searchInsert(array,array.length,5));
+
+
+    }
+
+    static int searchInsert(int A[], int n, int target) {
+        if(n < 1) {
+            return 0;
+        }
+
+        int low = 0;
+        int high = n-1;
+        int mid;
+        int pos=0;
+        while(low <= high) {
+            mid = low + (high-low)/2;
+
+            if(A[mid] == target) {
+                return mid;
+            } else if(A[mid] > target) {
+                high = mid - 1;
+                pos = mid;
+            } else {
+                low = mid + 1;
+                pos = mid + 1;
+            }
+        }
+
+        return pos;
+    }
+    
 }
